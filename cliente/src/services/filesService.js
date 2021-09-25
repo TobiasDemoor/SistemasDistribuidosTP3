639590850483b5e95c2900ce4b1e3d96c54c544b
Leopoldo Lening Celaya <57147656+pololeningcelaya@users.noreply.github.com>
2021-09-25 18:26:40 -0300

    add main components
    Co-authored-by: Tobías Demoor <TobiasDemoor@users.noreply.github.com>

---
+++ cliente/src/services/filesService.js
@@ -0,0 +1,36 @@
+
+export async function getFileList(){
+    // TODO: replace placeholder
+    return [
+        {
+            id:1,
+            name: "Sims 4 full con todas las expansiones"
+        },
+        {
+            id:2,
+            name: "Work and Travel"
+        },
+        {
+            id:3,
+            name: "Gol del diego a los ingleses"
+        },
+        {
+            id:4,
+            name: "Gol de maradona"
+        },
+        {
+            id:5,
+            name: "El diegote con la del mundo"
+        }
+    ];
+}
+
+export async function getFile(file) {
+    // TODO: replace placeholder
+    console.log(file);
+}
+
+export async function sendNewFile(name, size, nodeIP, nodePort){
+    // TODO: replace placeholder
+    console.log(name, size, nodeIP, nodePort);
+}
